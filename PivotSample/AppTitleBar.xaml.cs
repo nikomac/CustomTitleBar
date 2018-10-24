d95643ac53f1e8e149361d7b1f19b8fdec963bba
Guillem Tizne Mena <guillem.tizne@virtusway.com>
2018-10-24 11:58:59 +0200

    First commit

---
+++ PivotSample/AppTitleBar.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using Windows.ApplicationModel.Core;
+
+namespace PivotSample
+{
+    public partial class AppTitleBar : UserControl
+    {
+
+        public AppTitleBar()
+        {
+            InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            // Set XAML element as a draggable region.
+            Window.Current.SetTitleBar(TitleBarField);
+
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            this.Height = coreTitleBar.Height;
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+                this.Visibility = Visibility.Visible;
+            else
+                this.Visibility = Visibility.Collapsed;
+            
+        }
+
+
+    }
+}
